544e15fbe3d0c19dd5171cd7b35d379af3a63672
MOSHOOD OLAWALE <shabimoshood07@gmail.com>
2025-07-15 11:08:45 +0100

    Initial commit

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>SignInddddddddddddddddddddddddddddddddddddddd</Text>
+    </View>
+  );
+};
+
+export default SignIn;
