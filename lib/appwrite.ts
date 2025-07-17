5e922c33561e7f481183a73e2346d717347ad0e3
MOSHOOD OLAWALE <shabimoshood07@gmail.com>
2025-07-17 21:27:59 +0100

    Feature: auth pages

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,7 +1,29 @@
+import { CreateUserParams } from "@/type";
+import { Account, Avatars, Client, Databases } from "react-native-appwrite";
+
 export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: "com.moshcode.food-delivery",
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   databaseId: "68762cd0003560ca6863",
   userCollectionId: "68762dc6003def6e2029",
+};
+
+export const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint!)
+  .setProject(appwriteConfig.projectId!)
+  .setPlatform(appwriteConfig.platform!);
+
+export const account = new Account(client);
+export const database = new Databases(client);
+export const avatars = new Avatars(client);
+
+export const createUser = async ({
+  name,
+  email,
+  password,
+}: CreateUserParams) => {
+  
 };
